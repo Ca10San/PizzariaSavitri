fd6da1bf34ffe0b31e4a750ddd5fe65cb4c6ff2e
Ca10San <caiodarkus2@gmail.com>
2022-06-24 22:51:18 -0300

    :art: Frontend - Refactor: improve code

--- packages/frontend/modules/internal/views/CSelfService/_internals/components/c-available-pizzas.vue
+++ packages/frontend/modules/internal/views/CSelfService/_internals/components/c-available-pizzas.vue
@@ -1,44 +1,39 @@
 <template>
-    <h2 class="text-xl text-slate-100 text-semibold">Pizzas</h2>
-    <ul class="flex justify-center space-x-4">
+    <h2 class='text-xl text-slate-100 text-semibold'>Pizzas</h2>
+    <ul class='flex justify-center space-x-4'>
         <c-cards-grid
-            :cards="buildCardsList(items)"
-            cardBtnText="Adicionar"
+            :cards='buildCardsList(items)'
+            cardBtnText='Adicionar'
         ></c-cards-grid>
     </ul>
 </template>
 
-<script setup lang="ts">
-import { useModule, toRefs } from "@savitri/frontend"
-import { reactive, onMounted } from "vue"
-import { useStore } from "vuex"
+<script setup lang='ts'>
+import { useModule, toRefs } from '@savitri/frontend'
+import { reactive, onMounted } from 'vue'
+import { useStore } from 'vuex'
 
-import { CCardsGrid } from "../../../../components/organisms"
+import { CCardsGrid } from '../../../../components/organisms'
 
 const store = useStore()
-const orderModule = reactive(useModule('order', store));
-const pizzaModule = reactive(useModule('pizza', store));
+const orderModule = reactive(useModule('order', store))
+const pizzaModule = reactive(useModule('pizza', store))
 
 onMounted(() => {
-    pizzaModule.getAll();
+    pizzaModule.getAll()
 })
 
 const {
     items
-} = toRefs(pizzaModule);
+} = toRefs(pizzaModule)
 
-function textSpecificFunction(testData) {
-    console.log(this);
-      
-    console.log(testData);
+async function textSpecificFunction(testData) {
+    console.log(this.store)
+    console.log(testData)
 }
 
 function buildDescription(ingredients: Array<Object<T>>) {
-    const builtDescription = ingredients.reduce((result, ingredient) => {
-        return result === "" ? ingredient.name : `${result}, ${ingredient.name}`;
-    }, "");
-
-    return builtDescription;
+    return ingredients.map((item) => item.name).join(', ')
 }
 
 function buildPricing(price: Number) {
@@ -46,19 +41,13 @@ function buildPricing(price: Number) {
 }
 
 function buildCardsList(itemsList) {
-    const builtCardsList = itemsList.reduce((result, item) => {
-        const newCardIndex = {
-            cardKey: item._id,
-            cardTitle: item.name,
-            cardDescription: buildDescription(item.ingredients),
-            cardDetailDescription: buildPricing(item.price_small.toString()),
-            cardBtnFunction: textSpecificFunction.bind(this, item.name)
-        };
-
-        result.push(newCardIndex);        
-        return result;
-    }, []);    
-    return builtCardsList;
+    return itemsList.map((item) => ({
+        cardKey: item._id,
+        cardTitle: item.name,
+        cardDescription: buildDescription(item.ingredients),
+        cardDetailDescription: buildPricing(item.price_small.toString()),
+        cardBtnFunction: textSpecificFunction.bind(this, item.name)
+    }))
 }
 
 </script>
